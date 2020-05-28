772027b6fa6efdcc2a8442f1eff45976987bbb13
gabrielnunes25 <62342222+gabrielnunes25@users.noreply.github.com>
2020-05-28 15:26:42 -0300

    Atualizando o script
    Adicionando novas funções

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
+var vidaMonstro = 10
 function game(){
     //Campo de descrição
-    var description = window.document.getElementById('descricao')
+    var descricao = window.document.getElementById('descricao')
     //Campo que recebi o comando do Usuario
-    var comand = window.document.getElementById('txtcomando')
+    var comando = document.getElementById('txtcomando')
     //Itens do campo de status
-    var life = window.document.getElementById('life')
-    var strong = window.document.getElementById('strong')
-    var shield = window.document.getElementById('shield')
-    var potion = window.document.getElementById('potion')
+    var vida = window.document.getElementById('vida')
+    var forca = window.document.getElementById('forca')
+    var escudo = window.document.getElementById('escudo')
+    var pocao = window.document.getElementById('pocao')
+    //Status do monstro
 
 
     // window.alert(`${comando.value} Teste 2`)
 
-    description.innerHTML = `${comand.value}`
-    //life.innerHTML = Number.parseInt(life.textContent) + 1
-    comand.value = ''
-}
+    descricao.innerHTML = `${comando.value} - `
+    // vida.innerHTML = Number.parseInt(vida.textContent) + 1
 
+    //Criando Monstro
+    if (vidaMonstro < 0){
+        vidaMonstro = 15
+    }
+    
+    //Escolhas
+    if (Number(comando.value) == 1) {
+        atacar()
+        descricao.innerText += `Vida do monstro ${vidaMonstro} `
+    }
+    else if (comando.value ==2) {
+        descricao.innerText += `Correu`
+    }
+
+    //Limpar o campo de entrada
+    comando.value = ''
+}
+function atacar(){
+    descricao.innerHTML += `Vida do Heroi - ${vida.textContent} - `
+    vidaMonstro = vidaMonstro - Number(forca.textContent)
+
+}
