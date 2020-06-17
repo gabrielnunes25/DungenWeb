1dcb7157c92c18cb0262df73089d7b6f9c1c49ec
gabrielnunes25 <62342222+gabrielnunes25@users.noreply.github.com>
2020-06-17 14:59:38 -0300

    Site Responsivo
    Adicionei a tag @media para quando a largura da tela for menor que 800px

--- script.js
+++ script.js
@@ -1,41 +1,44 @@
-var vidaMonstro = 10
+var vidaMonstro = 10;
+var status = 0;
+var level = 1;
+
 function game(){
     //Campo de descrição
-    var descricao = window.document.getElementById('descricao')
+    var descricao = window.document.getElementById('descricao');
     //Campo que recebi o comando do Usuario
-    var comando = document.getElementById('txtcomando')
+    var comando = document.getElementById('txtcomando');
     //Itens do campo de status
-    var vida = window.document.getElementById('vida')
-    var forca = window.document.getElementById('forca')
-    var escudo = window.document.getElementById('escudo')
-    var pocao = window.document.getElementById('pocao')
+    var vida = window.document.getElementById('vida');
+    var forca = window.document.getElementById('forca');
+    var escudo = window.document.getElementById('escudo');
+    var pocao = window.document.getElementById('pocao');
     //Status do monstro
 
 
     // window.alert(comando.value + 'Teste 2')
 
-    descricao.innerHTML = comando.value + ' -'
+    descricao.innerHTML = comando.value + ' -';
     // vida.innerHTML = Number.parseInt(vida.textContent) + 1
 
     //Criando Monstro
     if (vidaMonstro < 0){
-        vidaMonstro = 15
+        vidaMonstro = 15;
     }
     
     //Escolhas
     if (Number(comando.value) == 1) {
-        atacar()
-        descricao.innerText += ' Vida do monstro ' + vidaMonstro
+        atacar();
+        descricao.innerText += ' Vida do monstro ' + vidaMonstro;
     }
     else if (comando.value ==2) {
-        descricao.innerText += ' Correu'
+        descricao.innerText += ' Correu';
     }
 
     //Limpar o campo de entrada
-    comando.value = ''
+    comando.value = '';
 }
 
 function atacar(){
     descricao.innerHTML += ' Vida do Heroi - ' + vida.textContent + ' -'
-    vidaMonstro = vidaMonstro - Number(forca.textContent)
+    vidaMonstro = vidaMonstro - Number(forca.textContent);
 }
