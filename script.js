a5f3e7c60e8a06a4af62fee95fa7a86b5c62adb8
gabrielnunes25 <62342222+gabrielnunes25@users.noreply.github.com>
2020-06-18 16:59:04 -0300

    Ajustes nas fases
    Fiz uma forma de mudar de telas, talvez não seja a melhor, mas funciona :)

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var monstro = {
     escudo: 5,
     nro: 0,
 }
-var status = 0;
+var status = 1;
 var level = 1;
 
 function game(){
@@ -18,35 +18,69 @@ function game(){
     var escudo = window.document.getElementById('escudo');
     var pocao = window.document.getElementById('pocao');
 
-    descricao.innerHTML = comando.value.toUpperCase() + ' -';
-
     //Criando Monstro
     if (monstro.vida <= 0){
         monstro.vida = getRandom(7, 15);
         monstro.ataque = getRandom(2, 4);
         monstro.nro += 1;
     }
-    
-    //Escolhas
-    switch (comando.value.toUpperCase()) {
-        case '1':
-            descricao.innerHTML += ' Vida do monstro ' + monstro.nro + ' antes do ataque: ' + monstro.vida ;
-            atacar();
-            descricao.innerHTML += '<br/> Vida do monstro ' + monstro.nro + ' depois do ataque: ' + monstro.vida;
+
+    // STATUS
+    switch (Number(status)){
+        // TUTORIAL
+        case 1:
+            descricao.innerHTML = 'Digite <strong>1</strong> para <strong>atacar</strong> o monstro ou <strong>2</strong> para <strong>correr</strong> caso você esteja com medo. <br/><br/><span class="aviso">Precione ENTER ou CLIQUE EM ENVIAR para continuar!</span>'
+            status = 2;
             break;
-        case '2':
-            vida.textContent -= 4;
-            descricao.innerHTML += ' Correu e Apanhou';
+
+        // HISTÓRIA INICIAL
+        case 2:
+            descricao.innerHTML = 'Você entrou em uma dungeon! <br/> Tome cuidado, pois tem alguns monstros que podem te atacar. <br/><br/><span class="aviso">Precione ENTER ou CLIQUE EM ENVIAR para continuar!</span>'
+            status = 3;
             break;
-        default:
-            console.log('Não tem esse')
+
+        // PARTIDA
+        case 3:
+            comando.disabled = false;
+            descricao.innerHTML = comando.value.toUpperCase() + ' -';
+
+            //Escolhas
+            switch (comando.value.toUpperCase()) {
+                // ATACOU
+                case '1':
+                    descricao.innerHTML += ' Vida do monstro ' + monstro.nro + ' antes do ataque: ' + monstro.vida ;
+                    atacar();
+                    descricao.innerHTML += '<br/> Vida do monstro ' + monstro.nro + ' depois do ataque: ' + monstro.vida;
+                    break;
+                
+                // CORREU
+                case '2':
+                    vida.textContent -= 4;
+                    descricao.innerHTML += ' Correu e Apanhou';
+                    break;
+                
+                    // NÃO ESCOLHEU NENHUM
+                default:
+                    console.log('Não tem esse')
+                    break;
+            }
+            if(vida.textContent <= 0){
+                status = 4;
+            }
             break;
-        
+
+        // FINAL DO JOGO
+        case 4:
+            descricao.innerHTML = 'VOCÊ FOI DERROTADO';
+            vida.textContent = 10;
+            status = 2;
+            break;
     }
 
     if(vida.textContent <= 0){
         descricao.innerHTML = 'VOCÊ FOI DERROTADO';
         vida.textContent = 10;
+        status = 2;
     }
     
     //Limpar o campo de entrada
@@ -54,7 +88,7 @@ function game(){
 }
 
 function getRandom(min, max){
-    return Math.floor(Math.random() * max + min);
+    return Math.floor((Math.random() * (max - min)) + min);
 }
 
 function atacar(){
